9f1c0854cf692fbf3df05f7f1d98a718e775bc36
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2024-07-28 11:42:43 +0530

    reh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 const App = () => {
