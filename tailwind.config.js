d9c1c0a5a4a758d178a428241d466e7f8748d99c
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2025-01-20 02:25:28 +0530

    ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
