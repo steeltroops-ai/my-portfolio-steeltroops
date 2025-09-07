0f23ba95d1e10b343531171f75d4ec1a60261fa8
Mayank <87810082+steeltroops-ai@users.noreply.github.com>
2025-09-07 06:45:50 +0530

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,56 @@
-import logo from '../assets/mps.png';
+import logo from "../assets/mps.png";
 import { AiOutlineLink } from "react-icons/ai";
 import { FaXTwitter, FaLinkedinIn } from "react-icons/fa6";
 import { FiGithub, FiInstagram } from "react-icons/fi";
-
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-6 mb-16">
       <div className="flex items-center flex-shrink-0">
-        <img className="w-24 mx-0 -ml-4 -mr-4" src={logo} alt="logo"/>
+        <img className="w-24 mx-0 -ml-4 -mr-4" src={logo} alt="logo" />
       </div>
       <div className="flex justify-center gap-4 -mr-4 text-2xl ems-center">
-        <a href='https://x.com/steeltroops_ai' target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-cyan-300">
+        <a
+          href="https://x.com/steeltroops_ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-cyan-400"
+        >
           <FaXTwitter />
         </a>
-        <a href='https://github.com/steeltroops-ai' target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-cyan-300">
+        <a
+          href="https://github.com/steeltroops-ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-cyan-400"
+        >
           <FiGithub />
         </a>
-        <a href='https://instagram.com/steeltroops_ai' target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-cyan-300">
+        <a
+          href="https://instagram.com/steeltroops_ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-cyan-400"
+        >
           <FiInstagram />
         </a>
-        <a href='https://linkedin.com/in/steeltroops-ai' target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-cyan-300">
+        <a
+          href="https://linkedin.com/in/steeltroops-ai"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-cyan-400"
+        >
           <FaLinkedinIn />
         </a>
-        <a href='https://bento.me/steeltroops' target="_blank" rel="noopener noreferrer" className="cursor-pointer hover:text-cyan-300">
+        <a
+          href="https://bento.me/steeltroops"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer hover:text-cyan-400"
+        >
           <AiOutlineLink />
         </a>
-        </div>
+      </div>
     </nav>
   );
 };
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,7 +2,13 @@ import { RiReactjsLine } from "react-icons/ri";
 import { TbBrandNextjs, TbBrandTypescript } from "react-icons/tb";
 import { PiFileCppDuotone } from "react-icons/pi";
 import { GrJava } from "react-icons/gr";
-import { SiMongodb, SiPytorch, SiTensorflow, SiUnrealengine, SiNestjs } from "react-icons/si";
+import {
+  SiMongodb,
+  SiPytorch,
+  SiTensorflow,
+  SiUnrealengine,
+  SiNestjs,
+} from "react-icons/si";
 import { FaNodeJs, FaRust, FaGitAlt } from "react-icons/fa";
 import { FaGolang } from "react-icons/fa6";
 import { DiPython, DiDocker, DiPostgresql } from "react-icons/di";
@@ -10,170 +16,190 @@ import { LiaAws } from "react-icons/lia";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-    initials:{y:-10},
-    animate:{
-        y:[10,-10],
-        transition:{
-            duration:duration,
-            ease:"linear",
-            repeat:Infinity,
-            repeatType: "reverse",
-        }
-    }  
+  initials: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
 });
 
 const Technologies = () => {
   return (
-    <div className="pb-24 border-b border-neutral-800">
-      <motion.h2 
-      whileInView={{opacity:1,y:0}}
-      initial={{opacity:0,y:-100}}
-      transition={{duration:1.2}}
-      className="my-20 text-4xl text-center">Technologies</motion.h2>
-      <motion.div 
-        whileInView={{opacity:1,x:0}}
-        initial={{opacity:0,x:-100}}
-        transition={{duration:1.5}}
-        className="flex flex-wrap items-center justify-center gap-5">
-
+    <div className="pb-4 border-b border-neutral-900 lg:mb-3">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.8 }}
+        className="my-20 text-4xl text-center"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-5"
+      >
         {/* Programming Languages */}
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <DiPython className="text-blue-500 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <DiPython className="text-blue-500 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <FaGolang className="text-7xl text-cyan-500" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <FaGolang className="text-7xl text-cyan-500" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <PiFileCppDuotone className="text-blue-700 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <PiFileCppDuotone className="text-blue-700 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <TbBrandTypescript className="text-blue-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <TbBrandTypescript className="text-blue-600 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <GrJava className="text-red-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <GrJava className="text-red-600 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <FaRust className="text-orange-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <FaRust className="text-orange-600 text-7xl" />
         </motion.div>
 
         {/* Unreal Engine */}
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <SiUnrealengine className="text-white text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <SiUnrealengine className="text-white text-7xl" />
         </motion.div>
-        
+
         {/* Machine Learning */}
-        <motion.div 
+        <motion.div
           variants={iconVariants(4)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <SiTensorflow className="text-yellow-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <SiTensorflow className="text-yellow-600 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(6)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <SiPytorch className="text-red-600 text-7xl" />          
-        </motion.div> 
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <SiPytorch className="text-red-600 text-7xl" />
+        </motion.div>
 
         {/* Web Development */}
-        <motion.div 
+        <motion.div
           variants={iconVariants(2.5)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <RiReactjsLine className="text-7xl text-cyan-400" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(3)}
           initial="initial"
-          animate="animate" 
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <TbBrandNextjs className="text-white text-7xl" />          
+          animate="animate"
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <TbBrandNextjs className="text-white text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(3)}
           initial="initial"
-          animate="animate" 
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <SiNestjs className="text-red-600 text-7xl" />          
+          animate="animate"
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <SiNestjs className="text-red-600 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(5)}
           initial="initial"
-          animate="animate" 
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <SiMongodb className="text-green-500 text-7xl" />          
+          animate="animate"
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <SiMongodb className="text-green-500 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(5)}
           initial="initial"
-          animate="animate" 
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <DiPostgresql className="text-blue-500 text-7xl" />          
+          animate="animate"
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <DiPostgresql className="text-blue-500 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(2)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <FaNodeJs className="text-green-500 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <FaNodeJs className="text-green-500 text-7xl" />
         </motion.div>
 
-        <motion.div 
+        <motion.div
           variants={iconVariants(2)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <LiaAws className="text-orange-500 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <LiaAws className="text-orange-500 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(2)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <FaGitAlt className="text-red-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <FaGitAlt className="text-red-600 text-7xl" />
         </motion.div>
-        <motion.div 
+        <motion.div
           variants={iconVariants(2)}
           initial="initial"
           animate="animate"
-          className="p-4 border-4 border-neutral-800 rounded-2xl">
-          <DiDocker className="text-blue-600 text-7xl" />          
+          className="p-4 border-4 border-neutral-800 rounded-2xl"
+        >
+          <DiDocker className="text-blue-600 text-7xl" />
         </motion.div>
-        
-      </motion.div>       
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
 export default Technologies;
